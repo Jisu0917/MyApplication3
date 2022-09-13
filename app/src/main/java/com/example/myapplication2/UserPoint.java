0ae480f75ac875041806220397c1b75bab2c6557
Jisu <bjisu8099@gmail.com>
2022-09-13 22:47:30 +0900

    포인트 제도 오류 수정

--- app/src/main/java/com/example/myapplication2/UserPoint.java
+++ app/src/main/java/com/example/myapplication2/UserPoint.java
@@ -4,23 +4,22 @@ import android.app.Application;
 import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class UserPoint extends Application {
-    public static String HISTORY_DIR = "sookpeech_history";
+    public static String HISTORY_DIR = "";
 
     private Long userId;
     private int user_point;
@@ -39,28 +38,28 @@ public class UserPoint extends Application {
 //        }
 
         //////
-        // 버전 30 이상에서 작동하도록 수정
-        if (Build.VERSION.SDK_INT >= 30){
-            System.out.println("android version >= 30");
-
-            File destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-            if (!destination.exists()) { // 원하는 경로에 폴더가 있는지 확인
-                destination.mkdirs();
-                Log.d("UserPoint", "destination Created");
-            }
-
-            directory = new File(destination + File.separator+HISTORY_DIR);
-            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
-                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
-                Log.d("UserPoint", "Directory Created");
-            }
-        } else{
-            directory = new File(Environment.getExternalStorageDirectory() + File.separator+HISTORY_DIR);
-            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
-                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
-                Log.d("UserPoint", "Directory Created");
-            }
-        }
+//        // 버전 30 이상에서 작동하도록 수정
+//        if (Build.VERSION.SDK_INT >= 30){
+//            System.out.println("android version >= 30");
+//
+//            File destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+//            if (!destination.exists()) { // 원하는 경로에 폴더가 있는지 확인
+//                destination.mkdirs();
+//                Log.d("UserPoint", "destination Created");
+//            }
+//
+//            directory = new File(destination + File.separator+HISTORY_DIR);
+//            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
+//                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
+//                Log.d("UserPoint", "Directory Created");
+//            }
+//        } else{
+//            directory = new File(Environment.getExternalStorageDirectory() + File.separator+HISTORY_DIR);
+//            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
+//                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
+//                Log.d("UserPoint", "Directory Created");
+//            }
+//        }
 
         //////
     }
@@ -107,26 +106,57 @@ public class UserPoint extends Application {
 
     private void addHistoryToFile(String str) {
 
+//        System.out.println("Environment.getExternalStorageState()=" + Environment.getExternalStorageState());
+        this.HISTORY_DIR = "sookpeech_history_user" + userId;
+
+        // 버전 30 이상에서 작동하도록 수정
+        if (Build.VERSION.SDK_INT >= 30){
+            System.out.println("android version >= 30");
+
+            File destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            if (!destination.exists()) { // 원하는 경로에 폴더가 있는지 확인
+                destination.mkdirs();
+                Log.d("UserPoint", "destination Created");
+            }
+
+            directory = new File(destination + File.separator+HISTORY_DIR);
+            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
+                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
+                Log.d("UserPoint", "Directory Created");
+            }
+        } else{
+            directory = new File(Environment.getExternalStorageDirectory() + File.separator+HISTORY_DIR);
+            if (!directory.exists()) { // 원하는 경로에 폴더가 있는지 확인
+                directory.mkdirs();  // 해당 유저의 history 파일이 없으면
+                Log.d("UserPoint", "Directory Created");
+            }
+        }
+
+
         this.filename = "user"+userId+"_history.txt";
-        saveFile = new File(directory, filename);
+//        saveFile = new File(directory, filename);
+        saveFile = new File(directory.getPath() + File.separator + filename);
+
+//        if (!saveFile.exists()) {
+//            try {
+//                boolean newFile = saveFile.createNewFile();
+//                System.out.println("newFile : " + newFile);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
         if (str.equals("join")) {
-            System.out.println("addHistoryToFile : join");  //임시, 확인용
             if (!saveFile.exists()) {
-                System.out.println("addHistoryToFile : join : saveFile.exists() false");  //임시, 확인용
-
-                //write on the file
                 try {
-                    BufferedWriter buf = new BufferedWriter(new FileWriter(saveFile, true));
-                    buf.append("회원가입을 축하합니다!#30 포인트 지급#남은 포인트 : 30");
-                    buf.newLine(); // 개행
-                    buf.close();
+                    FileWriter writer = new FileWriter(saveFile);
+                    writer.append("회원가입을 축하합니다!#30 포인트 지급#남은 포인트 : 30" + "\r\n");
+                    writer.flush();
+                    writer.close();
+//            Toast.makeText(UserPoint.this, "Saved", Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            } else {
-
-                System.out.println("addHistoryToFile : join : saveFile.exists() true");  //임시, 확인용
             }
         } else {
 
@@ -161,15 +191,15 @@ public class UserPoint extends Application {
                 System.out.println("getHistory : saveFile.exists()=true");  //임시, 확인용
 
                 String line = null; // 한줄씩 읽기
-            try {
-                BufferedReader buf = new BufferedReader(new FileReader(saveFile));
-                while ((line = buf.readLine()) != null) {
-                    history.add(line);
+                try {
+                    BufferedReader buf = new BufferedReader(new FileReader(saveFile));
+                    while ((line = buf.readLine()) != null) {
+                        history.add(line);
+                    }
+                    buf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                buf.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
 //                Scanner scan = null;
 //                try {
